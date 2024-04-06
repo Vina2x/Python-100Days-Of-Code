95f02ec270aed90b9143b805a86dd28ba8763168
Vina <162609442+Vina2x@users.noreply.github.com>
2024-04-06 23:18:16 +0530

    Add files via upload

---
+++ Day 9 Dictionary and Nesting/nestingDictAndList.py
@@ -0,0 +1,19 @@
+# Nesting dictionary in a dictionary
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lillie", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visted": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 22,},
+}
+
+# Nesting dictionary in a list
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lillie", "Dijon"],
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visted": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 22,
+    },
+]
